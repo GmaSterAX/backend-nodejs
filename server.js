21d233ee249cac72fcaf68080aef71da333149c5
remzi sağlık <rmzzmr46@gmail.com>
2026-07-20 18:30:42 +0300

    Stage 3: create with validation

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import { triggerAsyncId } from "node:async_hooks";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 
 let tasks = [{id: 1, title: "Laundary", done: false},
         {id: 2, title: "Coding NodeJS", done: true}, 
@@ -38,12 +40,28 @@ app.get("/task/:id", (req, res) => {
     const task = tasks.find((t) => t.id === id);
 
     if (!task) {
-        return res.status(400).json({ error: `Task ${id} not found!`});
+        return res.status(404).json({ error: `Task ${id} not found!`});
     }
 
     res.status(200).json(task);
 });
 
+//POST /tasks - create a new task
+app.post("/tasks", (req, res) => {
+    const {title} = req.body;
+    if(!title || typeof title !== 'string' || title.trim() === '') {
+        return res.status(400).json({ error: "Title is required and cannot be empty!"});
+    }
+
+    //const nextId = tasks.length > 0 ? Math.max(...tasks.map((t) => t.id)) + 1 : 1;
+    const nextId = tasks.length + 1;
+    
+    const newTask = {id: nextId, title: title.trim(), done: false};
+    tasks.push(newTask);
+
+    res.status(201).json(newTask);
+
+});
 
 app.listen(port, () => {
     console.log(`App is listenin on port ${port}`);
