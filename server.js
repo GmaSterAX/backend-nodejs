983899167a29d4adc9e29b4000b0e49af9ee5625
remzi sağlık <rmzzmr46@gmail.com>
2026-07-20 17:31:55 +0300

    Stage1: root and health endpoints

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/", (req, res) => {
+    res.json({
+        name: "Task API",
+        version: "1.0",
+        endpoints: ["/tasks"]
+    });
+});
+
+app.get("/health", (req, res) => {
+res.json({status: "ok"});
 });
 
 app.listen(port, () => {
     console.log(`App is listenin on port ${port}`);
 });
-
